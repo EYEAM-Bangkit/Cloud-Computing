cf16a9fc35df7a5545d539a666d3139e1de31432
Muhammad Ilham Hanifan <the.ilhamhanifan@gmail.com>
2022-05-28 14:01:20 +0700

    UPDATE classifier.py

--- flask_rest_api/app/classifier.py
+++ flask_rest_api/app/classifier.py
@@ -1,1 +1,18 @@
-#cloud sql code#
+from keras.models import load_model
+from keras.preprocessing import image
+from numpy import expand_dims, vstack, argmax
+
+model = load_model("./app/models/model_1.h5")
+
+# classifier code and stuffs
+def classify(data):
+    img = data.resize((300,300))
+    x = image.img_to_array(img)
+    x = expand_dims(x, axis=0)
+    images = vstack([x])
+
+    classi = argmax(model.predict(images, batch_size=10), axis=-1)[0]
+
+    labels = {0: 'Binturong', 1: 'Koala', 2: 'Lemur', 3: 'Tarsier', 4:'Walrus'}
+    result = labels[classi]
+    return result
