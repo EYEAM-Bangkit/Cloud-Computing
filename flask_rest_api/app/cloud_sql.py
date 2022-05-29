12309a454871cd0b3b483c544eb21b156c4b77fd
Ikhari <iqbalkhariza2@gmail.com>
2022-05-29 15:12:26 +0700

    Up

--- flask_rest_api/app/cloud_sql.py
+++ flask_rest_api/app/cloud_sql.py
@@ -1,1 +1,26 @@
 #cloud sql code#
+import mysql.connector
+
+# establishing the connection
+conn = mysql.connector.connect(
+    host = "10.39.0.3",       # e.g. '172.0.0.1'
+    user = "root",            # e.g. 'user'
+    password = "caca",        # e.g. 'password'
+    database = "hewankudb"    # e.g. 'mydatabase'
+)
+
+# Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+# Retrieving single row
+sql = '''SELECT * from hewankusayang'''
+
+# Executing the query
+cursor.execute(sql)
+
+# Fetching 1st row from the table
+result = cursor.fetchmany(size=2)
+print(result)
+
+# Closing the connection
+conn.close()
