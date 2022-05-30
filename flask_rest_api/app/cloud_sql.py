fe3c518ff2e62fdbd2778386d1c7ea8881217709
Ikhari <iqbalkhariza2@gmail.com>
2022-05-30 18:57:40 +0700

    up

--- flask_rest_api/app/cloud_sql.py
+++ flask_rest_api/app/cloud_sql.py
@@ -1,26 +1,28 @@
 #cloud sql code#
 import mysql.connector
+import json
 
 # establishing the connection
 conn = mysql.connector.connect(
-    host = "34.101.110.162",       # e.g. '172.0.0.1'
+    host = "10.39.0.3",       # e.g. '172.0.0.1'
     user = "root",            # e.g. 'user'
     password = "caca",        # e.g. 'password'
     database = "hewankudb"    # e.g. 'mydatabase'
 )
 
-# Creating a cursor object using the cursor() method
-cursor = conn.cursor()
+# check the connection
+if conn:
+    print("Connected Successfully")
+else:
+    print("Connection Not Established")
 
-# Retrieving single row
-sql = '''SELECT * from hewankusayang'''
+# Fetching the data
+def getSpeciesData(name):
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM hewankusayang WHERE namapopuler = '{name}'")
+    # Just change the column using "where"
+    result = cursor.fetchone() 
+    # Result index 2 must be a null foto
+    return json.dumps({"namapopuler": result[0], "namailmiah": result[1], "foto": result[2], "taxonomy": result[3], "kingdom": result[4], "genus": result[5], "class": result[6], "ordo": result[7], "family": result[8], "species": result[9], "deskripsi": result[10], "persebaran": result[11], "habitat": result[12], "iucn": result[13], "tersedia": result[14], "rataumur": result[15], "ratapanjang": result[16], "ratalebar": result[17], "rataberat": result[18]})
 
-# Executing the query
-cursor.execute(sql)
-
-# Fetching 1st row from the table
-result = cursor.fetchone()
-print(result)
-
-# Closing the connection
-conn.close()
+print(getSpeciesData('nama2'))
