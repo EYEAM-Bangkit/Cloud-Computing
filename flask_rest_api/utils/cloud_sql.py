50ed858bbcea99f756ea83f521dd455fb24bce77
Ikhari <iqbalkhariza2@gmail.com>
2022-05-31 09:59:27 +0700

    Update using sqlalchemy

--- flask_rest_api/utils/cloud_sql.py
+++ flask_rest_api/utils/cloud_sql.py
@@ -1,28 +1,46 @@
 #cloud sql code#
-import mysql.connector
 import json
+from sqlalchemy import create_engine
 
 # establishing the connection
-conn = mysql.connector.connect(
-    host = "10.39.0.3",       # e.g. '172.0.0.1'
-    user = "root",            # e.g. 'user'
-    password = "caca",        # e.g. 'password'
-    database = "hewankudb"    # e.g. 'mydatabase'
-)
+engine = create_engine('mysql+mysqldb://root:caca@10.39.0.3:3306/hewankudb')
+connection = engine.raw_connection()
 
 # check the connection
-if conn:
+if connection:
     print("Connected Successfully")
 else:
     print("Connection Not Established")
 
 # Fetching the data
 def getSpeciesData(name):
-    cursor = conn.cursor()
+    cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM hewankusayang WHERE namapopuler = '{name}'")
     # Just change the column using "where"
-    result = cursor.fetchone() 
-    # Result index 2 must be a null foto
-    return json.dumps({"namapopuler": result[0], "namailmiah": result[1], "foto": result[2], "taxonomy": result[3], "kingdom": result[4], "genus": result[5], "class": result[6], "ordo": result[7], "family": result[8], "species": result[9], "deskripsi": result[10], "persebaran": result[11], "habitat": result[12], "iucn": result[13], "tersedia": result[14], "rataumur": result[15], "ratapanjang": result[16], "ratalebar": result[17], "rataberat": result[18]})
+    result = cursor.fetchone()
+    return json.dumps(
+        {
+            "namapopuler": result[0],
+            "namailmiah": result[1],
+            "foto": result[2],          # Result index 2 must be a null foto
+            "taxonomy": result[3],
+            "kingdom": result[4],
+            "genus": result[5],
+            "class": result[6],
+            "ordo": result[7],
+            "family": result[8],
+            "species": result[9],
+            "deskripsi": result[10],
+            "persebaran": result[11],
+            "habitat": result[12],
+            "iucn": result[13],
+            "tersedia": result[14],
+            "rataumur": result[15],
+            "ratapanjang": result[16],
+            "ratalebar": result[17],
+            "rataberat": result[18]
+        }
+    )
+
 
 print(getSpeciesData('nama2'))
