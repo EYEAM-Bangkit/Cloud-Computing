ffb077007b907c4e6dc944631d046602b2859944
Muhammad Ilham Hanifan <the.ilhamhanifan@gmail.com>
2022-06-09 01:00:31 +0700

    add loguser service

---
+++ flask-rest-api/loguser-functions/loguser.py
@@ -0,0 +1,39 @@
+import base64,json
+import pymysql
+import pytz
+import datetime
+
+def connect_db():
+    conn = pymysql.connect(
+        host='10.39.1.3', 
+        user='root', 
+        password='caca', 
+        database='hewankudb', 
+        cursorclass = pymysql.cursors.DictCursor
+    )
+    return conn
+
+def get_time_now():
+    tz = pytz.timezone('Asia/Jakarta')
+    return datetime.datetime.now(tz)
+
+def loguser(event, context):
+    msg = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+    eventid = context.event_id
+    userid  = msg['userid']
+    animal  = msg['animal']
+    logtime = get_time_now()
+
+    print(f"eventid {eventid}, userid {userid}, animal {animal}, logtime {logtime}")
+
+    conn   = connect_db()
+    cursor = conn.cursor()
+
+    sql ="""INSERT INTO user values (
+            %s, %s, %s, %s)
+         """
+         
+    cursor.execute(sql,(eventid,userid,animal,logtime))
+    conn.commit()
+    conn.close()
+    
