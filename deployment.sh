84b5aff58ef3f488f56ccbd269e08062dff29945
Muhammad Ilham Hanifan <the.ilhamhanifan@gmail.com>
2022-06-09 00:59:58 +0700

    update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,7 @@
 #DO NOT EXECUTE THIS FILE#
+
+#// Build and Deploy Cloud Run Containers
+
 GOOGLE_CLOUD_PROJECT=glossy-chimera-350206
 
 gcloud builds submit --tag gcr.io/glossy-chimera-350206/cloudsql:1.0.0 .
@@ -17,6 +20,13 @@ gcloud run deploy --image=gcr.io/glossy-chimera-350206/mlclassifier:1.0.0 \
 --max-instances=1 \
 --allow-unauthenticated \
 --memory=4Gi
+
+#// Setup Pub Sub and Cloud Function with pub/sub trigger
+gcloud pubsub topics create eyeam-loguser-topic
+
+# function name = eyeam-loguser
+
+#// Setup API Gateway
 
 #create service account and it's private key
 
