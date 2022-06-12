37a8b66916cfcccacc93d078d16ac58e999932b5
Muhammad Ilham Hanifan <the.ilhamhanifan@gmail.com>
2022-06-12 15:21:29 +0700

    add pubsub.py

--- flask-rest-api/loguser-functions/pubsub.py
+++ flask-rest-api/loguser-functions/pubsub.py
@@ -1,16 +1,16 @@
-#testing code for triggering the cloud function
+#testing code to publish pubsub msg for triggering the cloud function.
 from google.cloud import pubsub_v1
 
 def publish_msg():
     publisher = pubsub_v1.PublisherClient()
 
-    project_id = "glossy-chimera-350206"
-    topic_id = "eyeam-loguser-topic"
+    project_id = "[project-id]"
+    topic_id = "[topic-id]"
 
     topic_path = publisher.topic_path(project_id, topic_id)
 
     data_str = f'{"userid": "Stranger","animal": "Monkey"}'
     data = data_str.encode("utf-8")
     future = publisher.publish(topic_path, data)
-
+    print('published')
     return True
